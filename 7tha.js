1599274920df8409d2bd569b327f62c059c87df2
schoolpauls <schoolpauls123@gmail.com>
2024-09-02 00:17:56 +0530

    Update 7tha.js

--- 7tha.js
+++ 7tha.js
@@ -1,120 +1,120 @@
 function navigateToPage1() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage2() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage3() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage4() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage5() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage6() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage7() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage8() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage9() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage10() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage11() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage12() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage13() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage14() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage15() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage16() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage17() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage18() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage19() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage20() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage21() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage22() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage23() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage24() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage25() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage26() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage27() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage28() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage29() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
 function navigateToPage30() {
-    window.location.href = ''; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/login.html'; // Link to the desired page
 }
 
